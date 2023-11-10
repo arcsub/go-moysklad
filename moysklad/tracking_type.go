9c03f4623731516e99ff0bcb744e9a3f7dc45ff0
arcsub <79924091218@ya.ru>
2023-11-10 20:38:36 +0500

    feat: внесены изменения API от 26-10-2023

--- moysklad/tracking_type.go
+++ moysklad/tracking_type.go
@@ -5,6 +5,7 @@ package moysklad
 type TrackingType string
 
 const (
+	TrackingTypeBeerAlcohol TrackingType = "BEER_ALCOHOL" // [26-10-2023]
 	TrackingTypeElectronics TrackingType = "ELECTRONICS"
 	TrackingTypeClothes     TrackingType = "LP_CLOTHES"
 	TrackingTypeLinens      TrackingType = "LP_LINENS"
