31c5c0c9e4fbac651f7f696f61a3adc4a77cea47
arcsub <79924091218@ya.ru>
2024-05-19 15:53:19 +0500

    bump: v0.0.36

--- moysklad/moysklad.go
+++ moysklad/moysklad.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	Version          = "v0.0.35"
+	Version          = "v0.0.36"
 	baseApiURL       = "https://api.moysklad.ru/api/remap/1.2/"
 	defaultUserAgent = "go-moysklad/" + Version
 
