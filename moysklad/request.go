5264b8607a68b6c7c6bbc3306bc18133016c5432
arcsub <79924091218@ya.ru>
2024-05-19 11:39:47 +0500

    fix: исправлен вывод ошибок запросов

--- moysklad/request.go
+++ moysklad/request.go
@@ -31,53 +31,38 @@ func (rb *RequestBuilder[T]) SetParams(params *Params) *RequestBuilder[T] {
 	return rb
 }
 
-func (rb *RequestBuilder[T]) Get(ctx context.Context) (*T, *resty.Response, error) {
-	data := new(T)
+func (rb *RequestBuilder[T]) send(ctx context.Context, method string, body any) (*T, *resty.Response, error) {
+	result := new(T)
 	apiErrors := new(ApiErrors)
-	resp, err := rb.req.SetContext(ctx).SetResult(data).SetError(apiErrors).Get(rb.uri)
 
-	if resp.Error() != nil {
-		return data, resp, apiErrors
+	resp, err := rb.req.SetContext(ctx).SetBody(body).SetResult(result).SetError(apiErrors).Execute(method, rb.uri)
+
+	if err != nil {
+		return nil, resp, err
 	}
 
-	return data, resp, err
+	if resp.IsError() {
+		return nil, resp, apiErrors
+	}
+
+	return result, resp, nil
+}
+
+func (rb *RequestBuilder[T]) Get(ctx context.Context) (*T, *resty.Response, error) {
+	return rb.send(ctx, http.MethodGet, nil)
 }
 
 func (rb *RequestBuilder[T]) Put(ctx context.Context, body any) (*T, *resty.Response, error) {
-	data := new(T)
-	apiErrors := new(ApiErrors)
-	resp, err := rb.req.SetContext(ctx).SetBody(body).SetResult(data).SetError(apiErrors).Put(rb.uri)
-
-	if resp.Error() != nil {
-		return data, resp, apiErrors
-	}
-
-	return data, resp, err
+	return rb.send(ctx, http.MethodPut, body)
 }
 
 func (rb *RequestBuilder[T]) Post(ctx context.Context, body any) (*T, *resty.Response, error) {
-	data := new(T)
-	apiErrors := new(ApiErrors)
-	resp, err := rb.req.SetContext(ctx).SetBody(body).SetResult(data).SetError(apiErrors).Post(rb.uri)
-
-	if resp.Error() != nil {
-		return data, resp, apiErrors
-	}
-
-	return data, resp, err
+	return rb.send(ctx, http.MethodPost, body)
 }
 
 func (rb *RequestBuilder[T]) Delete(ctx context.Context) (bool, *resty.Response, error) {
-	apiErrors := new(ApiErrors)
-	resp, err := rb.req.SetContext(ctx).SetError(apiErrors).Delete(rb.uri)
-
-	ok := resp.StatusCode() == http.StatusOK
-
-	if resp.Error() != nil {
-		return ok, resp, apiErrors
-	}
-
-	return ok, resp, err
+	_, resp, err := rb.send(ctx, http.MethodDelete, nil)
+	return resp.StatusCode() == http.StatusOK, resp, err
 }
 
 func (rb *RequestBuilder[T]) Async(ctx context.Context) (*AsyncResultService[T], *resty.Response, error) {
