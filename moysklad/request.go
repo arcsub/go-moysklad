da4342e7d196f1898526dcdb745c59feccf5f91f
arcsub <79924091218@ya.ru>
2024-05-28 20:23:26 +0500

    fix: изменена логика обработки ошибок от сервиса МойСклад

--- moysklad/request.go
+++ moysklad/request.go
@@ -3,9 +3,9 @@ package moysklad
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"github.com/go-resty/resty/v2"
 	"github.com/google/go-querystring/query"
+	"log"
 	"net/http"
 	"reflect"
 	"strings"
@@ -29,6 +29,127 @@ func NewRequestBuilder[T any](client *Client, uri string) *RequestBuilder[T] {
 	return &RequestBuilder[T]{client.R(), uri}
 }
 
+// TODO: improve
+func parseResponse[T any](r *resty.Response) (*T, *resty.Response, error) {
+	// check empty response body
+	if r.Body() == nil {
+		return nil, r, nil
+	}
+
+	var (
+		bodyBytes  = r.Body()
+		bodyString = string(bodyBytes)
+		result     = *new(T)
+		apiErrors  ApiErrors
+	)
+
+	// response body is slice
+	if strings.HasPrefix(bodyString, "[") && strings.HasSuffix(bodyString, "]") &&
+		reflect.ValueOf(result).Kind() == reflect.Slice {
+
+		switch statusCode := r.StatusCode(); {
+
+		case statusCode >= http.StatusOK && statusCode < http.StatusMultipleChoices: // ok
+			if err := json.Unmarshal(bodyBytes, &result); err != nil {
+				return nil, r, err
+			}
+
+		case statusCode >= http.StatusBadRequest: // error
+			var rawSlice []json.RawMessage
+			if err := json.Unmarshal(bodyBytes, &rawSlice); err != nil {
+				return nil, r, err
+			}
+
+			resultType := reflect.TypeOf(result)
+
+			if resultType.Kind() == reflect.Ptr {
+				resultType = resultType.Elem()
+			}
+
+			if resultType.Kind() == reflect.Slice {
+				resultType = resultType.Elem()
+			}
+
+			if resultType.Kind() != reflect.Struct {
+				log.Printf("[DEBIG] resultType is not a struct: %v", resultType.Kind())
+				return nil, r, nil
+			}
+
+			data := reflect.New(reflect.TypeOf(result)).Interface()
+			dataType := reflect.TypeOf(data)
+
+			if dataType.Kind() == reflect.Ptr {
+				dataType = dataType.Elem()
+			}
+
+			if dataType.Kind() == reflect.Slice {
+				dataType = dataType.Elem()
+			} else {
+				log.Printf("[DEBIG] dataType is %v", dataType.Kind())
+				return nil, r, nil
+			}
+
+			dataValue := reflect.ValueOf(data)
+			if dataValue.Kind() == reflect.Ptr {
+				dataValue = dataValue.Elem()
+			}
+
+			if dataType.Kind() == reflect.Ptr {
+				dataValue = dataValue.Elem()
+			}
+
+			elem := dataValue.Type().Elem()
+
+			if elem.Kind() == reflect.Ptr {
+				elem = elem.Elem()
+			}
+
+			if elem.Kind() == reflect.Slice {
+				elem = elem.Elem()
+			}
+
+			for _, object := range rawSlice {
+				newElem := reflect.New(elem).Elem()
+				if err := json.Unmarshal(object, newElem.Addr().Interface()); err == nil && !newElem.IsZero() {
+					if newElem.Kind() == reflect.Ptr {
+						newElem = newElem.Elem()
+					}
+
+					dataValue.Set(reflect.Append(dataValue, newElem))
+					continue
+				}
+
+				var errs ApiErrors
+				if err := json.Unmarshal(object, &errs); err == nil {
+					apiErrors.ApiErrors = append(apiErrors.ApiErrors, errs.ApiErrors...)
+				}
+			}
+
+			result = dataValue.Interface().(T)
+		}
+	} else { // response body is object
+		switch statusCode := r.StatusCode(); {
+
+		case statusCode >= http.StatusOK && statusCode < http.StatusMultipleChoices: // ok
+			if err := json.Unmarshal(bodyBytes, &result); err == nil &&
+				!reflect.ValueOf(&result).Elem().IsZero() {
+				return &result, r, nil
+			}
+
+		case statusCode >= http.StatusBadRequest: // error
+			if err := json.Unmarshal(bodyBytes, &apiErrors); err != nil {
+				return nil, r, err
+			}
+		}
+	}
+
+	if len(apiErrors.ApiErrors) > 0 {
+		return &result, r, apiErrors
+	}
+
+	return &result, r, nil
+}
+
 func (rb *RequestBuilder[T]) SetParams(params *Params) *RequestBuilder[T] {
 	v, _ := query.Values(params)
 	rb.req.SetQueryParamsFromValues(v)
@@ -36,20 +157,12 @@ func (rb *RequestBuilder[T]) SetParams(params *Params) *RequestBuilder[T] {
 }
 
 func (rb *RequestBuilder[T]) send(ctx context.Context, method string, body any) (*T, *resty.Response, error) {
-	result := new(T)
-	apiErrors := new(ApiErrors)
-
-	resp, err := rb.req.SetContext(ctx).SetBody(body).SetResult(result).SetError(apiErrors).Execute(method, rb.uri)
-
+	resp, err := rb.req.SetContext(ctx).SetBody(body).Execute(method, rb.uri)
 	if err != nil {
 		return nil, resp, err
 	}
 
-	if resp.IsError() {
-		return nil, resp, apiErrors
-	}
-
-	return result, resp, nil
+	return parseResponse[T](resp)
 }
 
 func (rb *RequestBuilder[T]) Get(ctx context.Context) (*T, *resty.Response, error) {
@@ -62,59 +175,6 @@ func (rb *RequestBuilder[T]) Put(ctx context.Context, body any) (*T, *resty.Resp
 
 func (rb *RequestBuilder[T]) Post(ctx context.Context, body any) (*T, *resty.Response, error) {
 	return rb.send(ctx, http.MethodPost, body)
-}
-
-func (rb *RequestBuilder[T]) PostMany(ctx context.Context, body any) ([]*T, *resty.Response, error) {
-	var result []*T
-
-	resp, err := rb.req.SetContext(ctx).SetBody(body).SetResult(result).Execute(http.MethodPost, rb.uri)
-	if err != nil {
-		return nil, resp, err
-	}
-
-	result, err = parsePostMany[T](resp.Body())
-
-	return result, resp, err
-}
-
-func parsePostMany[T any](b []byte) ([]*T, error) {
-	bytesToStr := string(b)
-
-	if bytesToStr == "" || bytesToStr == "[]" {
-		return nil, errors.New("parsePostMany: body is empty")
-	}
-
-	if !strings.HasPrefix(bytesToStr, "[") {
-		return nil, errors.New("parsePostMany: body is not an array")
-	}
-
-	var anyObjects []json.RawMessage
-	if err := json.Unmarshal(b, &anyObjects); err != nil {
-		return nil, err
-	}
-
-	var entities []*T
-	var apiErrors []ApiError
-
-	for _, object := range anyObjects {
-		t := *new(T)
-		if err := json.Unmarshal(object, &t); err == nil &&
-			!reflect.ValueOf(&t).Elem().IsZero() {
-			entities = append(entities, &t)
-			continue
-		}
-
-		var e ApiErrors
-		if err := json.Unmarshal(object, &e); err == nil {
-			apiErrors = append(apiErrors, e.ApiErrors...)
-		}
-	}
-
-	if len(apiErrors) > 0 {
-		return entities, ApiErrors{ApiErrors: apiErrors}
-	}
-
-	return entities, nil
 }
 
 func (rb *RequestBuilder[T]) Delete(ctx context.Context) (bool, *resty.Response, error) {
