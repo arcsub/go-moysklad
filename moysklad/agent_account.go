803681c0eaf9b8357b313bddd202fa32b24b9c42
arcsub <79924091218@ya.ru>
2024-05-30 18:14:07 +0500

    feat: добавлены Get/Set для AgentAccount

--- moysklad/agent_account.go
+++ moysklad/agent_account.go
@@ -19,6 +19,81 @@ type AgentAccount struct {
 	Updated              *Timestamp `json:"updated,omitempty"`              // Момент последнего обновления
 }
 
-func (a AgentAccount) String() string {
-	return Stringify(a)
+func (agentAccount AgentAccount) GetAccountID() uuid.UUID {
+	return Deref(agentAccount.AccountID)
+}
+
+func (agentAccount AgentAccount) GetAccountNumber() string {
+	return Deref(agentAccount.AccountNumber)
+}
+
+func (agentAccount AgentAccount) GetBankLocation() string {
+	return Deref(agentAccount.BankLocation)
+}
+
+func (agentAccount AgentAccount) GetBankName() string {
+	return Deref(agentAccount.BankName)
+}
+
+func (agentAccount AgentAccount) GetBic() string {
+	return Deref(agentAccount.Bic)
+}
+
+func (agentAccount AgentAccount) GetCorrespondentAccount() string {
+	return Deref(agentAccount.CorrespondentAccount)
+}
+
+func (agentAccount AgentAccount) GetID() uuid.UUID {
+	return Deref(agentAccount.ID)
+}
+
+func (agentAccount AgentAccount) GetIsDefault() bool {
+	return Deref(agentAccount.IsDefault)
+}
+
+func (agentAccount AgentAccount) GetMeta() Meta {
+	return Deref(agentAccount.Meta)
+}
+
+func (agentAccount AgentAccount) GetUpdated() Timestamp {
+	return Deref(agentAccount.Updated)
+}
+
+func (agentAccount *AgentAccount) SetAccountNumber(accountNumber string) *AgentAccount {
+	agentAccount.AccountNumber = &accountNumber
+	return agentAccount
+}
+
+func (agentAccount *AgentAccount) SetBankLocation(bankLocation string) *AgentAccount {
+	agentAccount.BankLocation = &bankLocation
+	return agentAccount
+}
+
+func (agentAccount *AgentAccount) SetBankName(bankName string) *AgentAccount {
+	agentAccount.BankName = &bankName
+	return agentAccount
+}
+
+func (agentAccount *AgentAccount) SetBic(bic string) *AgentAccount {
+	agentAccount.Bic = &bic
+	return agentAccount
+}
+
+func (agentAccount *AgentAccount) SetCorrespondentAccount(correspondentAccount string) *AgentAccount {
+	agentAccount.CorrespondentAccount = &correspondentAccount
+	return agentAccount
+}
+
+func (agentAccount *AgentAccount) SetIsDefault(isDefault bool) *AgentAccount {
+	agentAccount.IsDefault = &isDefault
+	return agentAccount
+}
+
+func (agentAccount *AgentAccount) SetMeta(meta *Meta) *AgentAccount {
+	agentAccount.Meta = meta
+	return agentAccount
+}
+
+func (agentAccount AgentAccount) String() string {
+	return Stringify(agentAccount)
 }
