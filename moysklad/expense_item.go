950d4646308ddd90eec064bba244f7c70e8b287b
arcsub <79924091218@ya.ru>
2024-06-07 22:35:36 +0500

    feat: добавлены Get/Set для expense_item.go

--- moysklad/expense_item.go
+++ moysklad/expense_item.go
@@ -20,11 +20,68 @@ type ExpenseItem struct {
 	Updated      *Timestamp `json:"updated,omitempty"`      // Момент последнего обновления сущности
 }
 
-func (e ExpenseItem) String() string {
-	return Stringify(e)
+func (expenseItem ExpenseItem) GetAccountID() uuid.UUID {
+	return Deref(expenseItem.AccountID)
 }
 
-func (e ExpenseItem) MetaType() MetaType {
+func (expenseItem ExpenseItem) GetCode() string {
+	return Deref(expenseItem.Code)
+}
+
+func (expenseItem ExpenseItem) GetDescription() string {
+	return Deref(expenseItem.Description)
+}
+
+func (expenseItem ExpenseItem) GetExternalCode() string {
+	return Deref(expenseItem.ExternalCode)
+}
+
+func (expenseItem ExpenseItem) GetID() uuid.UUID {
+	return Deref(expenseItem.ID)
+}
+
+func (expenseItem ExpenseItem) GetMeta() Meta {
+	return Deref(expenseItem.Meta)
+}
+
+func (expenseItem ExpenseItem) GetName() string {
+	return Deref(expenseItem.Name)
+}
+
+func (expenseItem *ExpenseItem) SetCode(code string) *ExpenseItem {
+	expenseItem.Code = &code
+	return expenseItem
+}
+
+func (expenseItem *ExpenseItem) SetDescription(description string) *ExpenseItem {
+	expenseItem.Description = &description
+	return expenseItem
+}
+
+func (expenseItem *ExpenseItem) SetExternalCode(externalCode string) *ExpenseItem {
+	expenseItem.ExternalCode = &externalCode
+	return expenseItem
+}
+
+func (expenseItem *ExpenseItem) SetMeta(meta *Meta) *ExpenseItem {
+	expenseItem.Meta = meta
+	return expenseItem
+}
+
+func (expenseItem *ExpenseItem) SetName(name string) *ExpenseItem {
+	expenseItem.Name = &name
+	return expenseItem
+}
+
+func (expenseItem ExpenseItem) GetUpdated() Timestamp {
+	return Deref(expenseItem.Updated)
+}
+
+func (expenseItem ExpenseItem) String() string {
+	return Stringify(expenseItem)
+}
+
+func (expenseItem ExpenseItem) MetaType() MetaType {
 	return MetaTypeExpenseItem
 }
 
