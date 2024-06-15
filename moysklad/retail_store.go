d3a1868176355391c0eb46f8ae3d3a8ea072333a
arcsub <79924091218@ya.ru>
2024-06-15 21:30:32 +0500

    code clean up

--- moysklad/retail_store.go
+++ moysklad/retail_store.go
@@ -86,6 +86,7 @@ type RetailStore struct {
 	CreateAgentsTags                    Slice[string]             `json:"createAgentsTags,omitempty"`
 }
 
+// Clean возвращает сущность с единственным заполненным полем Meta
 func (retailStore RetailStore) Clean() *RetailStore {
 	return &RetailStore{Meta: retailStore.Meta}
 }
@@ -703,6 +704,65 @@ func (retailStore RetailStore) String() string {
 
 func (retailStore RetailStore) MetaType() MetaType {
 	return MetaTypeRetailStore
+}
+
+// Cashier Кассир.
+// Ключевое слово: cashier
+// Документация МойСклад: https://dev.moysklad.ru/doc/api/remap/1.2/dictionaries/#suschnosti-kassir
+type Cashier struct {
+	AccountID   *uuid.UUID   `json:"accountId,omitempty"`   // ID учетной записи
+	Employee    *Employee    `json:"employee,omitempty"`    // Метаданные сотрудника, которого представляет собой кассир
+	ID          *uuid.UUID   `json:"id,omitempty"`          // ID сущности
+	Meta        *Meta        `json:"meta,omitempty"`        // Метаданные
+	RetailStore *RetailStore `json:"retailStore,omitempty"` // Метаданные точки продаж, к которой прикреплен кассир
+}
+
+// Clean возвращает сущность с единственным заполненным полем Meta
+func (cashier Cashier) Clean() *Cashier {
+	return &Cashier{Meta: cashier.Meta}
+}
+
+func (cashier Cashier) GetAccountID() uuid.UUID {
+	return Deref(cashier.AccountID)
+}
+
+func (cashier Cashier) GetEmployee() Employee {
+	return Deref(cashier.Employee)
+}
+
+func (cashier Cashier) GetID() uuid.UUID {
+	return Deref(cashier.ID)
+}
+
+func (cashier Cashier) GetMeta() Meta {
+	return Deref(cashier.Meta)
+}
+
+func (cashier Cashier) GetRetailStore() RetailStore {
+	return Deref(cashier.RetailStore)
+}
+
+func (cashier *Cashier) SetEmployee(employee *Employee) *Cashier {
+	cashier.Employee = employee.Clean()
+	return cashier
+}
+
+func (cashier *Cashier) SetMeta(meta *Meta) *Cashier {
+	cashier.Meta = meta
+	return cashier
+}
+
+func (cashier *Cashier) SetRetailStore(retailStore *RetailStore) *Cashier {
+	cashier.RetailStore = retailStore.Clean()
+	return cashier
+}
+
+func (cashier Cashier) String() string {
+	return Stringify(cashier)
+}
+
+func (cashier Cashier) MetaType() MetaType {
+	return MetaTypeCashier
 }
 
 // RetailStoreState Информация статусе точки продаж
