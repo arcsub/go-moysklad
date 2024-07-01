c0da83c2681c9f98593bfe9ce59c240159fde747
arcsub <79924091218@ya.ru>
2024-07-01 18:42:58 +0500

    doc добавлены комментарии в group.go, gtd.go

--- moysklad/group.go
+++ moysklad/group.go
@@ -7,91 +7,129 @@ import (
 )
 
 // Group Отдел.
-// Ключевое слово: group
-// Документация МойСклад: https://dev.moysklad.ru/doc/api/remap/1.2/dictionaries/#suschnosti-otdel
+//
+// Код сущности: group
+//
+// [Документация МойСклад]
+//
+// [Документация МойСклад]: https://dev.moysklad.ru/doc/api/remap/1.2/dictionaries/#suschnosti-otdel
 type Group struct {
-	AccountID *uuid.UUID `json:"accountId,omitempty"` // ID учетной записи
+	AccountID *uuid.UUID `json:"accountId,omitempty"` // ID учётной записи
 	ID        *uuid.UUID `json:"id,omitempty"`        // ID Отдела
 	Index     *int       `json:"index,omitempty"`     // Порядковый номер в списке отделов
 	Meta      *Meta      `json:"meta,omitempty"`      // Метаданные Отдела
 	Name      *string    `json:"name,omitempty"`      // Наименование Отдела
 }
 
-// Clean возвращает сущность с единственным заполненным полем Meta
+// Clean возвращает указатель на объект с единственным заполненным полем [Meta].
+//
+// Метод позволяет избавиться от лишних данных при передаче запроса.
 func (group Group) Clean() *Group {
+	if group.Meta == nil {
+		return nil
+	}
 	return &Group{Meta: group.Meta}
 }
 
+// GetAccountID возвращает ID учётной записи.
 func (group Group) GetAccountID() uuid.UUID {
 	return Deref(group.AccountID)
 }
 
+// GetID возвращает ID Отдела.
 func (group Group) GetID() uuid.UUID {
 	return Deref(group.ID)
 }
 
+// GetIndex возвращает Порядковый номер в списке отделов.
 func (group Group) GetIndex() int {
 	return Deref(group.Index)
 }
 
+// GetMeta возвращает Метаданные Отдела.
 func (group Group) GetMeta() Meta {
 	return Deref(group.Meta)
 }
 
+// GetName возвращает Наименование Отдела.
 func (group Group) GetName() string {
 	return Deref(group.Name)
 }
 
+// SetIndex устанавливает Порядковый номер в списке отделов.
 func (group *Group) SetIndex(index int) *Group {
 	group.Index = &index
 	return group
 }
 
+// SetMeta устанавливает Метаданные Отдела.
 func (group *Group) SetMeta(meta *Meta) *Group {
 	group.Meta = meta
 	return group
 }
 
+// SetName устанавливает Наименование Отдела.
 func (group *Group) SetName(name string) *Group {
 	group.Name = &name
 	return group
 }
 
+// String реализует интерфейс [fmt.Stringer].
 func (group Group) String() string {
 	return Stringify(group)
 }
 
-// MetaType возвращает тип сущности.
+// MetaType возвращает код сущности.
 func (Group) MetaType() MetaType {
 	return MetaTypeGroup
 }
 
 // Update shortcut
 func (group Group) Update(ctx context.Context, client *Client, params ...*Params) (*Group, *resty.Response, error) {
-	return client.Entity().Group().Update(ctx, group.GetID(), &group, params...)
+	return NewGroupService(client).Update(ctx, group.GetID(), &group, params...)
 }
 
 // Create shortcut
 func (group Group) Create(ctx context.Context, client *Client, params ...*Params) (*Group, *resty.Response, error) {
-	return client.Entity().Group().Create(ctx, &group, params...)
+	return NewGroupService(client).Create(ctx, &group, params...)
 }
 
 // Delete shortcut
 func (group Group) Delete(ctx context.Context, client *Client) (bool, *resty.Response, error) {
-	return client.Entity().Group().Delete(ctx, group.GetID())
+	return NewGroupService(client).Delete(ctx, group.GetID())
 }
 
-// GroupService
-// Сервис для работы с отделами.
+// GroupService описывает методы сервиса для работы с отделами.
 type GroupService interface {
+	// GetList выполняет запрос на получение списка отделов.
+	// Принимает контекст и опционально объект параметров запроса Params.
+	// Возвращает объект List.
 	GetList(ctx context.Context, params ...*Params) (*List[Group], *resty.Response, error)
+
+	// Create выполняет запрос на создание отдела.
+	// Обязательные поля для заполнения:
+	//	- name (Наименование отдела)
+	// Принимает контекст, отдел и опционально объект параметров запроса Params.
+	// Возвращает созданный отдел.
 	Create(ctx context.Context, group *Group, params ...*Params) (*Group, *resty.Response, error)
+
+	// Delete выполняет запрос на удаление отдела.
+	// Принимает контекст и ID отдела.
+	// Возвращает true в случае успешного удаления отдела.
 	Delete(ctx context.Context, id uuid.UUID) (bool, *resty.Response, error)
+
+	// GetByID выполняет запрос на получение отдельного отдела по ID.
+	// Принимает контекст, ID отдела и опционально объект параметров запроса Params.
+	// Возвращает найденный отдел.
 	GetByID(ctx context.Context, id uuid.UUID, params ...*Params) (*Group, *resty.Response, error)
+
+	// Update выполняет запрос на изменение отдела.
+	// Принимает контекст, отдел и опционально объект параметров запроса Params.
+	// Возвращает изменённый отдел.
 	Update(ctx context.Context, id uuid.UUID, group *Group, params ...*Params) (*Group, *resty.Response, error)
 }
 
+// NewGroupService принимает [Client] и возвращает сервис для работы с отделами.
 func NewGroupService(client *Client) GroupService {
-	e := NewEndpoint(client, "entity/group")
-	return newMainService[Group, any, any, any](e)
+	return newMainService[Group, any, any, any](NewEndpoint(client, "entity/group"))
 }
--- moysklad/gtd.go
+++ moysklad/gtd.go
@@ -1,20 +1,26 @@
 package moysklad
 
 // GTD Грузовая таможенная декларация (ГТД).
-// Документация МойСклад: https://dev.moysklad.ru/doc/api/remap/1.2/dictionaries/#suschnosti-gruzowaq-tamozhennaq-deklaraciq-gtd
+//
+// [Документация МойСклад]
+//
+// [Документация МойСклад]: https://dev.moysklad.ru/doc/api/remap/1.2/dictionaries/#suschnosti-gruzowaq-tamozhennaq-deklaraciq-gtd
 type GTD struct {
 	Name *string `json:"name"` // Номер ГТД
 }
 
+// GetName возвращает Номер ГТД.
 func (gtd GTD) GetName() string {
 	return Deref(gtd.Name)
 }
 
+// SetName устанавливает Номер ГТД.
 func (gtd *GTD) SetName(name string) *GTD {
 	gtd.Name = &name
 	return gtd
 }
 
+// String реализует интерфейс [fmt.Stringer].
 func (gtd GTD) String() string {
 	return Stringify(gtd)
 }
