ab3b8cc39ea4c511a9420655eda2d2c298c5c4d7
arcsub <79924091218@ya.ru>
2024-07-03 15:07:30 +0500

    doc добавлены комментарии в named_filter.go

--- moysklad/named_filter.go
+++ moysklad/named_filter.go
@@ -5,46 +5,61 @@ import (
 )
 
 // NamedFilter Сохраненный фильтр.
-// Ключевое слово: namedfilter
-// Документация МойСклад: https://dev.moysklad.ru/doc/api/remap/1.2/dictionaries/#suschnosti-sohranennye-fil-try
+//
+// Код сущности: namedfilter
+//
+// [Документация МойСклад]
+//
+// [Документация МойСклад]: https://dev.moysklad.ru/doc/api/remap/1.2/dictionaries/#suschnosti-sohranennye-fil-try
 type NamedFilter struct {
-	AccountID *uuid.UUID `json:"accountId,omitempty"` // ID учетной записи
-	ID        *uuid.UUID `json:"id,omitempty"`        // ID сущности
-	Meta      *Meta      `json:"meta,omitempty"`      // Метаданные
-	Name      *string    `json:"name,omitempty"`      // Наименование
-	Owner     *Employee  `json:"owner,omitempty"`     // Владелец (Сотрудник)
+	AccountID *uuid.UUID `json:"accountId,omitempty"` // ID учётной записи
+	ID        *uuid.UUID `json:"id,omitempty"`        // ID фильтра
+	Meta      *Meta      `json:"meta,omitempty"`      // Метаданные фильтра
+	Name      *string    `json:"name,omitempty"`      // Наименование фильтра
+	Owner     *Employee  `json:"owner,omitempty"`     // Метаданные владельца (Сотрудника)
 }
 
-// Clean возвращает сущность с единственным заполненным полем Meta
+// Clean возвращает указатель на объект с единственным заполненным полем [Meta].
+//
+// Метод позволяет избавиться от лишних данных при передаче запроса.
 func (namedFilter NamedFilter) Clean() *NamedFilter {
+	if namedFilter.Meta == nil {
+		return nil
+	}
 	return &NamedFilter{Meta: namedFilter.Meta}
 }
 
+// GetAccountID возвращает ID учётной записи.
 func (namedFilter NamedFilter) GetAccountID() uuid.UUID {
 	return Deref(namedFilter.AccountID)
 }
 
+// GetID возвращает ID фильтра.
 func (namedFilter NamedFilter) GetID() uuid.UUID {
 	return Deref(namedFilter.ID)
 }
 
+// GetMeta возвращает Метаданные фильтра.
 func (namedFilter NamedFilter) GetMeta() Meta {
 	return Deref(namedFilter.Meta)
 }
 
+// GetName возвращает Наименование фильтра.
 func (namedFilter NamedFilter) GetName() string {
 	return Deref(namedFilter.Name)
 }
 
+// GetOwner возвращает Метаданные владельца (Сотрудника).
 func (namedFilter NamedFilter) GetOwner() Employee {
 	return Deref(namedFilter.Owner)
 }
 
+// String реализует интерфейс [fmt.Stringer].
 func (namedFilter NamedFilter) String() string {
 	return Stringify(namedFilter)
 }
 
-// MetaType возвращает тип сущности.
+// MetaType возвращает код сущности.
 func (NamedFilter) MetaType() MetaType {
 	return MetaTypeNamedFilter
 }
