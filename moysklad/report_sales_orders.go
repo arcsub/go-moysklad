69be59399812e2ea23d99395ff30debfb5b8a7a3
arcsub <79924091218@ya.ru>
2024-07-08 20:46:47 +0500

    doc добавлены комментарии в report_sales_orders.go

--- moysklad/report_sales_orders.go
+++ moysklad/report_sales_orders.go
@@ -6,45 +6,62 @@ import (
 )
 
 // SeriesElement Показатели (series).
-// Документация МойСклад: https://dev.moysklad.ru/doc/api/remap/1.2/reports/#otchety-pokazateli-prodazh-i-zakazow-pokazateli-series
+//
+// [Документация МойСклад]
+//
+// [Документация МойСклад]: https://dev.moysklad.ru/doc/api/remap/1.2/reports/#otchety-pokazateli-prodazh-i-zakazow-pokazateli-series
 type SeriesElement struct {
-	Date     Timestamp `json:"date"`
-	Sum      float64   `json:"sum"`
-	Quantity float64   `json:"quantity"`
+	Date     Timestamp `json:"date"`     // Дата
+	Sum      float64   `json:"sum"`      // Количество
+	Quantity float64   `json:"quantity"` // Сумма
 }
 
 // SalesPlotSeries Показатели продаж.
-// Ключевое слово: salesplotseries
-// Документация МойСклад: https://dev.moysklad.ru/doc/api/remap/1.2/reports/#otchety-pokazateli-prodazh-i-zakazow-pokazateli-prodazh
+//
+// Код сущности: salesplotseries
+//
+// [Документация МойСклад]
+//
+// [Документация МойСклад]: https://dev.moysklad.ru/doc/api/remap/1.2/reports/#otchety-pokazateli-prodazh-i-zakazow-pokazateli-prodazh
 type SalesPlotSeries struct {
-	Context Context         `json:"context"`
-	Meta    Meta            `json:"meta"`
-	Series  []SeriesElement `json:"series"` // Массив показателей
+	Context Context         `json:"context"` // Метаданные о выполнившем запрос сотруднике
+	Meta    Meta            `json:"meta"`    // Метаданные запроса
+	Series  []SeriesElement `json:"series"`  // Массив показателей
 }
 
-// MetaType возвращает тип сущности.
+// MetaType возвращает код сущности.
 func (SalesPlotSeries) MetaType() MetaType {
 	return MetaTypeReportSales
 }
 
 // OrdersPlotSeries Показатели заказов.
-// Ключевое слово: ordersplotseries
-// Документация МойСклад: https://dev.moysklad.ru/doc/api/remap/1.2/reports/#otchety-pokazateli-prodazh-i-zakazow-pokazateli-zakazow
+//
+// Код сущности: ordersplotseries
+//
+// [Документация МойСклад]
+//
+// [Документация МойСклад]: https://dev.moysklad.ru/doc/api/remap/1.2/reports/#otchety-pokazateli-prodazh-i-zakazow-pokazateli-zakazow
 type OrdersPlotSeries struct {
-	Context Context         `json:"context"`
-	Meta    Meta            `json:"meta"`
-	Series  []SeriesElement `json:"series"` // Массив показателей
+	Context Context         `json:"context"` // Метаданные о выполнившем запрос сотруднике
+	Meta    Meta            `json:"meta"`    // Метаданные запроса
+	Series  []SeriesElement `json:"series"`  // Массив показателей
 }
 
-// MetaType возвращает тип сущности.
+// MetaType возвращает код сущности.
 func (OrdersPlotSeries) MetaType() MetaType {
 	return MetaTypeReportOrders
 }
 
-// ReportSalesService
-// Сервис для работы с отчётом "Показатели продаж".
+// ReportSalesService описывает методы сервиса для работы с отчётом Показатели продаж.
 type ReportSalesService interface {
+	// GetPlotSeries выполняет запрос на получение показателей продаж.
+	// Принимает контекст и опционально объект параметров запроса Params.
+	// Возвращает показатели продаж.
 	GetPlotSeries(ctx context.Context, params ...*Params) (*SalesPlotSeries, *resty.Response, error)
+
+	// GetPlotSeriesAsync выполняет запрос на получение показателей продаж (асинхронно).
+	// Принимает контекст и опционально объект параметров запроса Params.
+	// Возвращает сервис для работы с контекстом асинхронного запроса.
 	GetPlotSeriesAsync(ctx context.Context, params ...*Params) (AsyncResultService[SalesPlotSeries], *resty.Response, error)
 }
 
@@ -52,29 +69,29 @@ type reportSalesService struct {
 	Endpoint
 }
 
+// NewReportSalesService принимает [Client] и возвращает сервис для работы с отчётом Показатели продаж.
 func NewReportSalesService(client *Client) ReportSalesService {
-	e := NewEndpoint(client, "report/sales")
-	return &reportSalesService{e}
+	return &reportSalesService{NewEndpoint(client, "report/sales")}
 }
 
-// GetPlotSeries Запрос на получение показателей продаж.
-// Документация МойСклад: https://dev.moysklad.ru/doc/api/remap/1.2/reports/#otchety-pokazateli-prodazh-i-zakazow-pokazateli-zakazow
 func (service *reportSalesService) GetPlotSeries(ctx context.Context, params ...*Params) (*SalesPlotSeries, *resty.Response, error) {
-	path := "report/sales/plotseries"
-	return NewRequestBuilder[SalesPlotSeries](service.client, path).SetParams(params...).Get(ctx)
+	return NewRequestBuilder[SalesPlotSeries](service.client, "report/sales/plotseries").SetParams(params...).Get(ctx)
 }
 
-// GetPlotSeriesAsync Запрос на получение показателей продаж (асинхронно).
-// Документация МойСклад: https://dev.moysklad.ru/doc/api/remap/1.2/reports/#otchety-pokazateli-prodazh-i-zakazow-pokazateli-zakazow
 func (service *reportSalesService) GetPlotSeriesAsync(ctx context.Context, params ...*Params) (AsyncResultService[SalesPlotSeries], *resty.Response, error) {
-	path := "report/sales/plotseries"
-	return NewRequestBuilder[SalesPlotSeries](service.client, path).SetParams(params...).Async(ctx)
+	return NewRequestBuilder[SalesPlotSeries](service.client, "report/sales/plotseries").SetParams(params...).Async(ctx)
 }
 
-// ReportOrdersService
-// Сервис для работы с отчётом "Показатели заказов".
+// ReportOrdersService описывает методы сервиса для работы с отчётом Показатели заказов.
 type ReportOrdersService interface {
+	// GetPlotSeries выполняет запрос на получение показателей заказов.
+	// Принимает контекст и опционально объект параметров запроса Params.
+	// Возвращает показатели заказов.
 	GetPlotSeries(ctx context.Context, params ...*Params) (*OrdersPlotSeries, *resty.Response, error)
+
+	// GetPlotSeriesAsync выполняет запрос на получение показателей заказов (асинхронно).
+	// Принимает контекст и опционально объект параметров запроса Params.
+	// Возвращает сервис для работы с контекстом асинхронного запроса.
 	GetPlotSeriesAsync(ctx context.Context, params ...*Params) (AsyncResultService[OrdersPlotSeries], *resty.Response, error)
 }
 
@@ -82,21 +99,15 @@ type reportOrdersService struct {
 	Endpoint
 }
 
+// NewReportOrdersService принимает [Client] и возвращает сервис для работы с отчётом Показатели заказов.
 func NewReportOrdersService(client *Client) ReportOrdersService {
-	e := NewEndpoint(client, "report/orders")
-	return &reportOrdersService{e}
+	return &reportOrdersService{NewEndpoint(client, "report/orders")}
 }
 
-// GetPlotSeries Запрос на получение показателей заказов.
-// Документация МойСклад: https://dev.moysklad.ru/doc/api/remap/1.2/reports/#otchety-pokazateli-prodazh-i-zakazow-pokazateli-zakazow
 func (service *reportOrdersService) GetPlotSeries(ctx context.Context, params ...*Params) (*OrdersPlotSeries, *resty.Response, error) {
-	path := "report/orders/plotseries"
-	return NewRequestBuilder[OrdersPlotSeries](service.client, path).SetParams(params...).Get(ctx)
+	return NewRequestBuilder[OrdersPlotSeries](service.client, "report/orders/plotseries").SetParams(params...).Get(ctx)
 }
 
-// GetPlotSeriesAsync Запрос на получение показателей заказов (асинхронно).
-// Документация МойСклад: https://dev.moysklad.ru/doc/api/remap/1.2/reports/#otchety-pokazateli-prodazh-i-zakazow-pokazateli-zakazow
 func (service *reportOrdersService) GetPlotSeriesAsync(ctx context.Context, params ...*Params) (AsyncResultService[OrdersPlotSeries], *resty.Response, error) {
-	path := "report/orders/plotseries"
-	return NewRequestBuilder[OrdersPlotSeries](service.client, path).SetParams(params...).Async(ctx)
+	return NewRequestBuilder[OrdersPlotSeries](service.client, "report/orders/plotseries").SetParams(params...).Async(ctx)
 }
